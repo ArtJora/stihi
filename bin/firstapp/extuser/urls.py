12b4e32b26132649aca88ee14235d37f85d9c685
ArtJora <flash.kramatorsk@gmail.com>
2016-03-09 16:39:43 +0200

    Some commit message

--- bin/firstapp/extuser/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-
-
-urlpatterns = [
-    url(r'^login/', 'extuser.views.login'),
-    url(r'^logout/', 'extuser.views.logout'),
-    url(r'^register/', 'extuser.views.register'),
-]
