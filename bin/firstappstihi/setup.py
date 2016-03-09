e3ab6e708fc67462a24a1d2be85bfcb78e37141a
ArtJora <flash.kramatorsk@gmail.com>
2016-03-09 16:54:04 +0200

    Initialization2

--- bin/firstappstihi/setup.py
+++ bin/firstappstihi/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='firstapp',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+      description='regonly',
+      author='alt00x8fq',
+      author_email='alt00x8fq@gmail.com',
+      url='http://firstapp-regonly.rhcloud.com/',
       install_requires=['Django>=1.3'],
      )
